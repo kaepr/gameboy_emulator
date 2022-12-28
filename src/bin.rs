89abcb61c2c429ed7d510984863144bacbb68687
Shagun Agrawal <agrawalshagun07@gmail.com>
2022-12-28 19:58:11 +0530

    add cart read

--- src/bin.rs
+++ src/bin.rs
@@ -1,3 +1,13 @@
+use gameboy_emulator_lib::cartridge::{Cartridge};
+
 fn main() {
     println!("Gameboy emulator !");
+        
+    let rom_path = "../../projects/test_roms/Tetris.gb";
+
+    let cart = Cartridge::new(rom_path.to_string());
+
+    cart.data.iter().take(20).for_each(|x| {
+        println!("{}", x);
+    });
 }
--- src/lib/cartridge.rs
+++ src/lib/cartridge.rs
@@ -1,1 +1,17 @@
+use std::{fs, path::Path};
+
+pub struct Cartridge {
+    pub data: Vec<u8>,
+}
+
+impl Cartridge {
+    pub fn new(path: String) -> Self {
+        let path = Path::new(&path);
+        let data = fs::read(path).unwrap();
+
+        Cartridge { data }
+    }
+
+    pub fn cart_read(addr: u16) {}
+}
 
